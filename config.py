523ca43b127586b246d312ccc05b7d3a247d1a23
dorukayaz007 <91373556+dorukayaz007@users.noreply.github.com>
2021-09-25 16:19:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Merhaba {first}\n\nKanallarımızdan aldığın linklerle sana içerikleri sunabilirim.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1371278429").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
